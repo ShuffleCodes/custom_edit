6c14a124adf5c132d34d41638a56ef3918270caa
Dawid <shufflelua@gmail.com>
2021-07-27 23:22:51 +0200

    Update test.lua

--- custom_edit/test.lua
+++ custom_edit/test.lua
@@ -13,6 +13,6 @@ edit6=customedit.create("",830, 367, 547, 67,false,"Wpisz treść nr 6....",fals
 
 edit7=customedit.create("",830, 466, 547, 67,false,"Wpisz treść nr 7....",false,"font2",{255, 105, 180,255},{255,255,255,255},{0,0,0,150},{255,255,255,255},true,350)
 
-edit8=customedit.create("",830, 574, 547, 67,false,"Wpisz treść nr 8....",false,"font2",{255,255,255,255},{0,0,0,255},{255,0,0,150},{255,255,255,255},true,2)
+edit8=customedit.create("",830, 574, 547, 67,false,"Wpisz treść nr 9....",false,"font2",{255,255,255,255},{0,0,0,255},{255,0,0,150},{255,255,255,255},true,2)
 
 
